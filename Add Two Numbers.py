06014661a86c6d6004cabc19ac7bf8ab5482e669
Muhammed Köse <117511279+MuhammedKsee@users.noreply.github.com>
2024-01-19 15:51:15 +0300

    Delete Add Two Numbers.py

--- Add Two Numbers.py
+++
@@ -1,9 +0,0 @@
-
-num1 = 1.5
-num2 = 6.3
-
-sum = num1 + num2
-
-print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
-
-print('The sum is %.1f' %(float(input('Enter first number: ')) + float(input('Enter second number: '))))
